b8e8d68c2d46a3ef1c0493e6a61458d09ab32d86
Chitransh Prasanna <chitranshprasanna6666@gmail.com>
2024-07-26 14:29:06 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -15,10 +15,14 @@ const prepareAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
     app.use('/api',apiRoutes);
+    // const UserRepository = require('./repository/user-repository');
 
     app.listen(PORT, () => {
         console.log(`Server Started on Port: ${PORT}`);
 
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
          // const incomingpassword = '123456';
         // const user = await User.findByPk(3);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
