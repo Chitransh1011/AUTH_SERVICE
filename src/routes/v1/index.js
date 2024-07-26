c563d92d54bbcd0e4b93473ccb2032c10d61e7f6
Chitransh Prasanna <chitranshprasanna6666@gmail.com>
2024-07-26 16:07:33 +0530

    Added user isAuth function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -14,5 +14,8 @@ router.post(
     AuthRequestValidators.validateUserAuth,
     UserController.signIn
 );
-
+router.get(
+    '/isAuthenticated',
+    UserController.isAuthenticated
+)
 module.exports = router;
